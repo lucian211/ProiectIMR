d0c52e6bdbb3aa0c0b2c191dff48152358f97d4a
lucian211 <82049216+lucian211@users.noreply.github.com>
2023-12-16 07:04:19 +0200

    Tema 9
    Interactiune cu obiecte si locomotie

---
+++ Assets/MyScripts/GhostMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMovement : MonoBehaviour
+{
+
+    float low;
+    float hight; 
+    float target;
+    float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hight = transform.localPosition.y + 0.1f;
+        low = transform.localPosition.y - 0.1f;
+        target = hight;
+
+      
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, new Vector3(transform.localPosition.x, target, transform.localPosition.z), speed *0.1f* Time.deltaTime);
+        Debug.Log(target);
+        if (transform.localPosition.y >= hight)
+       {   low = transform.localPosition.y - 0.2f;
+            target = low;
+          
+        }
+        if (transform.localPosition.y <= low)
+        {
+            hight = transform.localPosition.y +0.2f;
+            target = hight;
+        }
+
+
+    }
+}
+
